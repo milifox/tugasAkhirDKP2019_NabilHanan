1cea0011c6ca3aed0d7a8103c67f8e94937a882d
milifox <nabilhanan@outlook.com>
2019-05-27 08:37:19 +0700

    Tugas Akhir Praktikum DKP 2019 To Do List Sederhana

---
+++ todolist_sederhana/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace todolist_sederhana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 204);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Tambah";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(376, 204);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 26);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Tambah";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(323, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(46, 258);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(188, 174);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(323, 258);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(185, 174);
+            this.textBox4.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(100, 470);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 26);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Selesai";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(376, 470);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 26);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Selesai";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(551, 552);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "To Do List Sederhana";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ todolist_sederhana/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace todolist_sederhana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Queue<string> list1 = new Queue<string>();
+        string data1;
+
+        Queue<string> list2 = new Queue<string>();
+        string data2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            data1 = textBox1.Text;
+            list1.Enqueue(data1);
+            textBox1.Clear();
+
+            textBox3.Text = list1.ElementAtOrDefault(0) + Environment.NewLine
+                    + list1.ElementAtOrDefault(1) + Environment.NewLine
+                    + list1.ElementAtOrDefault(2) + Environment.NewLine
+                    + list1.ElementAtOrDefault(3) + Environment.NewLine
+                    + list1.ElementAtOrDefault(4) + Environment.NewLine
+                    + list1.ElementAtOrDefault(5) + Environment.NewLine
+                    + list1.ElementAtOrDefault(6) + Environment.NewLine
+                    + list1.ElementAtOrDefault(7) + Environment.NewLine
+                    + list1.ElementAtOrDefault(8) + Environment.NewLine
+                    + list1.ElementAtOrDefault(9) + Environment.NewLine;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            data2 = textBox2.Text;
+            list2.Enqueue(data2);
+            textBox2.Clear();
+
+            textBox4.Text = list2.ElementAtOrDefault(0) + Environment.NewLine
+                    + list2.ElementAtOrDefault(1) + Environment.NewLine
+                    + list2.ElementAtOrDefault(2) + Environment.NewLine
+                    + list2.ElementAtOrDefault(3) + Environment.NewLine
+                    + list2.ElementAtOrDefault(4) + Environment.NewLine
+                    + list2.ElementAtOrDefault(5) + Environment.NewLine
+                    + list2.ElementAtOrDefault(6) + Environment.NewLine
+                    + list2.ElementAtOrDefault(7) + Environment.NewLine
+                    + list2.ElementAtOrDefault(8) + Environment.NewLine
+                    + list2.ElementAtOrDefault(9) + Environment.NewLine;
+        }
+
+        
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            int jumlah1 = list1.Count;
+
+            if (jumlah1 >= 1)
+            {
+                list1.Dequeue();
+
+                textBox3.Text = list1.ElementAtOrDefault(0) + Environment.NewLine
+                        + list1.ElementAtOrDefault(1) + Environment.NewLine
+                        + list1.ElementAtOrDefault(2) + Environment.NewLine
+                        + list1.ElementAtOrDefault(3) + Environment.NewLine
+                        + list1.ElementAtOrDefault(4) + Environment.NewLine
+                        + list1.ElementAtOrDefault(5) + Environment.NewLine
+                        + list1.ElementAtOrDefault(6) + Environment.NewLine
+                        + list1.ElementAtOrDefault(7) + Environment.NewLine
+                        + list1.ElementAtOrDefault(8) + Environment.NewLine
+                        + list1.ElementAtOrDefault(9) + Environment.NewLine;
+            }   
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            
+            int jumlah2 = list2.Count;
+
+            if (jumlah2 >= 1)
+            {
+                list2.Dequeue();
+
+                textBox4.Text = list2.ElementAtOrDefault(0) + Environment.NewLine
+                        + list2.ElementAtOrDefault(1) + Environment.NewLine
+                        + list2.ElementAtOrDefault(2) + Environment.NewLine
+                        + list2.ElementAtOrDefault(3) + Environment.NewLine
+                        + list2.ElementAtOrDefault(4) + Environment.NewLine
+                        + list2.ElementAtOrDefault(5) + Environment.NewLine
+                        + list2.ElementAtOrDefault(6) + Environment.NewLine
+                        + list2.ElementAtOrDefault(7) + Environment.NewLine
+                        + list2.ElementAtOrDefault(8) + Environment.NewLine
+                        + list2.ElementAtOrDefault(9) + Environment.NewLine;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
